d3d9ff84bdb9c793dce2ccc1fa46877dc56357bf
BilalChbanat <bilal.chbanat2003@gmail.com>
2023-12-24 15:36:14 +0100

    error404 is done

--- router.php
+++ router.php
@@ -1,17 +1,56 @@
 <?php
-class Router
+
+// namespace Routes\Routes;
+
+class Route
 {
-    private $routes = [];
-    public function define($routes)
+    private string $controller = 'MVC\Controllers\HomeController';
+    private string $method = 'index';
+    private array $params = array();
+
+    public function __construct()
     {
-        $this->routes = $routes;
+        $this->Sender();
     }
-    public function direct($uri, $method)
+
+    public function Sender(): void
     {
-        if (array_key_exists($uri, $this->routes[$method])) {
-            return $this->routes[$method][$uri];
-        } else {
-            throw new Exception('No route defined for this URI.');
+        $uri = $_SERVER['REQUEST_URI'] ?? '';
+        $uri = explode('/', trim(strtolower($uri), '/'));
+        unset($uri[0]);
+        if (!empty($uri[1])) {
+            $controller = ucwords($uri[1]) . 'Controller';
+            unset($uri[1]);
+            $controller = "MVC\Controllers\\" . $controller;
+            if (class_exists($controller)) {
+                $this->controller = $controller;
+            } else {
+                include '../app/View/error404.php';
+                exit;
+            }
         }
+
+        $class = $this->controller;
+        $objetController = new $class;
+
+        if (isset($uri[2])) {
+
+            $method = $uri[2];
+            unset($uri[2]);
+
+            if (method_exists($objetController, $method)) {
+                $this->method = $method;
+            } else {
+                include 'views/404/404.php';
+                // echo "error not fined";
+                exit;
+            }
+        }
+
+        if (isset($uri[3])) {
+            $this->params = array_values($uri);
+        }
+
+        call_user_func_array([$objetController, $this->method], $this->params);
     }
 }
