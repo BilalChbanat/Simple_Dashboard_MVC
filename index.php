9d16d534d24702de254bcd0a7dd8f3f570b328e8
BilalChbanat <bilal.chbanat2003@gmail.com>
2023-12-24 15:00:42 +0100

    install composer

--- index.php
+++ index.php
@@ -26,3 +26,26 @@ if (isset($_GET['action'])) {
             break;
     }
 }
+
+// session_start();
+// require_once 'router.php';
+// require_once 'autoloader.php';
+// $router = new Router();
+// $router->define([
+//     'GET'=>[
+//         'list'=>'controller/userController.php',
+//         'product/add'=>'controller/ProductAddController.php',
+//         'product/edit'=>'controller/ProductEditController.php',
+//         'product/delete'=>'controller/ProductDeleteController.php',
+//     ]
+// ]);
+// if(isset($_GET['url'])){
+//     // remove crud_oop/ from the uri
+//     $uri = trim($_GET['url'],'/');
+//     $method = $_SERVER['REQUEST_METHOD'];
+//     try{
+//         require_once $router->direct($uri,$method);
+//     }catch(Exception $e){
+//         echo $e->getMessage();
+//     }
+// }
